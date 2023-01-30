bf107ecd93e08d231400d763797bda0285d1f2dc
ivan volkov <iwolkkov@gmai.com>
2023-01-30 21:01:49 +0200

    add getters/setters

--- src/main/java/org/example/model/FamilyCar.java
+++ src/main/java/org/example/model/FamilyCar.java
@@ -2,4 +2,12 @@ package org.example.model;
 
 public class FamilyCar extends Car {
     private boolean hasKidsSeat;
+
+    public boolean isHasKidsSeat() {
+        return hasKidsSeat;
+    }
+
+    public void setHasKidsSeat(boolean hasKidsSeat) {
+        this.hasKidsSeat = hasKidsSeat;
+    }
 }
